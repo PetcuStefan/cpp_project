624b16340691e5eb7c740495ae7385a9def38f68
Petcu Stefan <stefanitapetcu@gmail.com>
2023-11-14 19:00:40 +0200

    part of class event

--- Project/Source.cpp
+++ Project/Source.cpp
@@ -1,7 +1,69 @@
 #include<iostream>
 using namespace std;
+class event
+{
+private:
+	char* eventName = new char[20];
+	char* address = new char[40];
+	int hour;
+	int minutes;
+
+public:
+	event()
+	{
+		eventName = nullptr;
+		address = nullptr;
+		hour = -1;
+		minutes = -1;
+	}
+
+	void setEventName(char *newEventName)
+	{
+		strcpy(this->eventName, newEventName);
+	}
+
+	void setAddress(char *newAddress)
+	{
+		strcpy(this->address, newAddress);
+	}
+	void setHour(int hour)
+	{
+		this->hour = hour;
+	}
+	void setMinutes(int minutes)
+	{
+		this->minutes = minutes;
+	}
+	char* getEventName()
+	{
+		char *copyEventName=new char[strlen(this->eventName)];
+		strcpy(copyEventName, this->eventName);
+		return copyEventName;
+	}
+	char* getAddress()
+	{
+		char* copyAddress = new char[strlen(this->address)];
+		strcpy(copyAddress, this->address);
+		return copyAddress;
+	}
+	int getHour()
+	{
+		return this->hour;
+	}
+	int getMinutes()
+	{
+		return this->minutes;
+	}
+	~event()
+	{
+		cout << endl << "Destr" << endl;
+		delete[] eventName;
+		delete[] address;
+	}
+};
 int main()
 {
-	cout << "Hello world!";
+	event ceva;
+	cout << ceva.getHour;
 	return 0;
 }
